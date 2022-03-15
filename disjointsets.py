792755109aab723e84865588f024dd73f93ca487
Jackson Kirk <jkirk445@gmail.com>
2022-03-15 12:57:28 -0400

    Update disjointsets.py

--- disjointsets.py
+++ disjointsets.py
@@ -1,7 +1,6 @@
 ##  Python Data Structures Library
 ##
-##  Copyright (C) 2017 Vincent A. Cicirello.
-##  http://www.cicirello.org/
+
 ##
 ##  This program is free software: you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
